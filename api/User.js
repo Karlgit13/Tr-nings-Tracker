bafbeedb7601726f4882dd9a34c54c5ecf3a06e0
Karlgit13 <karl.vareskog@gmail.com>
2024-02-25 15:03:06 +0100

    Reorder fields in userSchema definition

--- api/User.js
+++ api/User.js
@@ -2,8 +2,8 @@ import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true }
 });
 
 export default model('User', userSchema);
