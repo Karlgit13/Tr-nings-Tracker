ba94f77e0590c297e809fc6ee854e317a299626b
Karlgit13 <karl.vareskog@gmail.com>
2024-03-01 12:58:37 +0100

    Refactor Timer component to handle recovery time past current time

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,24 +2,39 @@ import { useEffect, useState } from "react";
 
 const Timer = ({ trainingEnd }) => {
   const [formattedTrainingEnd, setFormattedTrainingEnd] = useState("");
+  const [isRecoveryPast, setIsRecoveryPast] = useState(false);
 
   useEffect(() => {
-    if (trainingEnd) {
-      const end = new Date(trainingEnd).toLocaleString("sv-SE", {
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-      });
-      setFormattedTrainingEnd(end);
-    } else {
-      setFormattedTrainingEnd("");
-    }
+    const checkRecoveryTime = () => {
+      if (trainingEnd) {
+        const trainedUntil = new Date(trainingEnd);
+        const now = new Date();
+
+        if (trainedUntil < now) {
+          setIsRecoveryPast(true);
+          setFormattedTrainingEnd("");
+        } else {
+          setIsRecoveryPast(false);
+          const endFormatted = trainedUntil.toLocaleString("sv-SE", {
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+          setFormattedTrainingEnd(endFormatted);
+        }
+      } else {
+        setIsRecoveryPast(false);
+        setFormattedTrainingEnd("");
+      }
+    };
+
+    checkRecoveryTime();
   }, [trainingEnd]);
 
   return (
     <div className="Timer">
-      {formattedTrainingEnd.length > 0 && (
+      {!isRecoveryPast && formattedTrainingEnd.length > 0 && (
         <p className="p-timer">
           Återhämtning till -{" "}
           <span className="text-red-600 font-bold text-lg md:text-xl">
