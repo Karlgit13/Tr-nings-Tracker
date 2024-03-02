581538618d981dbee43d680a131b3a658eed2077
Karlgit13 <karl.vareskog@gmail.com>
2024-03-02 14:29:51 +0100

    Add weekly reports to SavedData component

--- src/components/SavedData.jsx
+++ src/components/SavedData.jsx
@@ -1,20 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { fetchUserWeeklyReport } from "./api";
 import { useMuscle } from "./MuscleContext";
+import Header from "./Header";
 
 const SavedData = () => {
   const { userId, isLoggedIn } = useMuscle();
+  const [weeklyReports, setWeeklyReports] = useState([]);
+
   useEffect(() => {
-    fetchUserWeeklyReport(userId);
+    const fetchData = async () => {
+      if (isLoggedIn) {
+        const reports = await fetchUserWeeklyReport(userId);
+        setWeeklyReports(reports);
+      }
+    };
+
+    fetchData();
   }, [userId, isLoggedIn]);
 
   return (
     <div>
-      <div></div>
-      <Link to={"/"}>
-        <button className="red-button">Tillbaka</button>
-      </Link>
+      <Header />
+
+      <div className="flex flex-col w-full place-items-center textShadow p-4">
+        {weeklyReports.length > 0 ? (
+          <div className="text-white bg-blue-500 w-[90vw] p-2 rounded">
+            {weeklyReports.map((report, index) => (
+              <div key={index}>
+                <h3 className="text-center">
+                  Vecka: <span className="text-red-500">{report.weekOf}</span>
+                </h3>
+                <ul className="flex flex-row justify-evenly p-2">
+                  {report.trainedMuscles.map((muscle, index) => (
+                    <li className="relative" key={index}>
+                      {muscle}{" "}
+                      <img
+                        className="w-5 absolute -right-7 top-1"
+                        src={require("../assets/check.png")}
+                        alt=""
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Ingen data.</p>
+        )}
+      </div>
     </div>
   );
 };
