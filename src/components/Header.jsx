ab4e55c5d64a080d36d246b2294fdb52abf30e69
Karlgit13 <karl.vareskog@gmail.com>
2024-03-04 09:35:59 +0100

    Add useEffect hook to handle click outside functionality in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Burgare from "../assets/icons8-menu-50.png";
 import Logo from "../assets/nylogo.webp";
 import { useMuscle } from "./MuscleContext";
@@ -9,6 +9,22 @@ import LiveClock from "react-live-clock";
 const Header = () => {
   const { isLoggedIn, setIsloggedIn, userId, setUserId } = useMuscle();
   const [isClicked, setIsClicked] = useState();
+
+  const handleClickOutside = (event) => {
+    if (isClicked && !event.target.closest(".dropdown-menu")) {
+      setIsClicked(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isClicked) {
+      window.addEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [isClicked]);
 
   const toggleBurgare = () => {
     setIsClicked(!isClicked);
@@ -38,6 +54,9 @@ const Header = () => {
       <div>
         <Link to={"/"}>
           <img
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
             className="w-12 h-15 rounded-full cursor-pointer"
             src={Logo}
             alt="logo"
@@ -55,14 +74,17 @@ const Header = () => {
         {isLoggedIn ? (
           <div>
             <img
-              onClick={toggleBurgare}
+              onClick={(e) => {
+                e.stopPropagation();
+                toggleBurgare();
+              }}
               src={Burgare}
               alt={Burgare}
               className=" w-12 cursor-pointer mr-1"
             />
 
             {isClicked && (
-              <div className="flex flex-col absolute p-8 bg-[#01112b] z-10 right-0 gap-1 -mr-5 rounded w-[200px] md:w-[350px]">
+              <div className="dropdown-menu flex flex-col absolute p-8 bg-[#01112b] z-10 right-0 gap-1 -mr-5 rounded w-[200px] md:w-[350px]">
                 <>
                   <Link to={"/data"}>
                     <button className="red-button w-full">Min Data</button>
